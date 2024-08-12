fix attention mask bugs

cllm/utils.py
@@ -210,7 +210,7 @@ def _prepare_decoder_attention_mask(
         combined_attention_mask = _make_causal_mask(
             input_shape,
             # inputs_embeds.dtype,
-            torch.float32,  # [MODIFIED] force to cast to float32
+            torch.bfloat16,  # [MODIFIED] force to cast to float32
             device=inputs_embeds.device,
             past_key_values_length=past_key_values_length,
         )

eval/gsm8k/speedup.py
@@ -104,7 +104,8 @@ def jacobian_speed_evaluate(processed_prompt, model, tokenizer, max_new_tokens, 
 def speed_compare(args):
     # Load model and tokenizer
     model = transformers.LlamaForCausalLM.from_pretrained(args.test_model_path, low_cpu_mem_usage=True, device_map='auto', 
-                                             torch_dtype=torch.bfloat16, attn_implementation="flash_attention_2")
+                                            #  torch_dtype=torch.bfloat16, attn_implementation="flash_attention_2")
+                                            torch_dtype=torch.bfloat16)
     tokenizer = transformers.AutoTokenizer.from_pretrained(
         args.teacher_model_path,
         padding_side="right",